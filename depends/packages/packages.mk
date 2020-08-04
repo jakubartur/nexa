ff99c59587a3da2321f17bcdafca9afe8f82d364
fanquake <fanquake@gmail.com>
2020-08-04 21:40:56 +0800

    build: add libxcb_util_image 0.4.0

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -5,8 +5,7 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms
-
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
